66f1f461a0954ffe3811c5b6fc9b819fe6a72b5f
jeongeum <wjddma1202@naver.com>
2022-02-14 22:53:25 +0900

    0214 #8 weather

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "1dc020747cfb98cfd4b8798e43b76b65";
+function onGeoOk(position) {
+  const lat = position.coords.latitude; // 위도 경도를 표시해줌
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.main.temp}°`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
